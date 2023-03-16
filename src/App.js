201d96195f6b535cd5223c68baf7d94593b2e0dd
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-16 12:20:10 +0000

    Added auth/logout to net-code + previous merge fixup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./Dashboard";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import UserRegister from "./views/UserRegister";
+import Login from './views/Login';
 import { Container } from "react-bootstrap";
 import NavigationBar from "./components/navbar.component";
 import Footer from "./components/footer.component";
+import APIClientServer from './API/APIClientServer';
+import { useState } from 'react';
 
 function App() {
+	const [token, setToken] = useState(window.localStorage.getItem('token'));
+	const client = new APIClientServer(() => token, logout, setError);
+
+	const navigate = useNavigate();
+
 	function login(token) {
 		window.localStorage.setItem('token', token);
 		setToken(token);
@@ -16,6 +24,7 @@ function App() {
 	function logout() {
 		window.localStorage.removeItem('token');
 		setToken(null);
+		navigate('/') ;
 	}
 
 	const [msgData, setMsgData] = useState({ msg: null, type: null });
--- src/services/netService.js
+++ src/services/netService.js
@@ -1,19 +1,30 @@
 import axios from "axios";
 
-const CONTENT_JSON_HEADER = {
-	headers: {
-		'content-type': 'application/json'
-	}
+const CONTENT_JSON = {
+	'content-type': 'application/json'
 } ;
 
 const API_URL = process.env.REACT_APP_API_URL ;
 
 export default class NetService {
-	constructor(errHandler) {
+	constructor({errHandler = null, tokenProvider = null, logoutHandler = null} = {}) {
+		console.log(arguments) ;
 		this.errHandler = errHandler ;
+		this.tokenProvider = tokenProvider ;
+		this.logoutHandler = logoutHandler ;
+
+		axios.defaults.withCredentials = true ; // Send cookies
 	}
 
 	errHandlerInternal(err) {
+		// If we got an unauthorized / forbidden status response then treat it as a log-out
+		if (err.response.status === 401 || err.response.status === 403) {
+			if (this.logoutHandler) {
+    		this.logoutHandler();
+				return ;
+			}
+  	}
+
 		if (this.errHandler) this.errHandler(err.message) ;
 		else console.error(err) ;
 		throw(err) ; // (rethrow)
@@ -23,20 +34,40 @@ export default class NetService {
 		if (this.errHandler) this.errHandler(null) ;
 	}
 
+	request(method, url, data = null, extraHeaders = {}) {
+		let headers = {}
+		if (this.tokenProvider) headers.token = this.tokenProvider() ;
+		headers = {...headers, ...extraHeaders} ;
+		console.log(headers) ;
+
+		return axios({method, url, data, headers}).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+	}
+
+	// Empty-body functions
 	get(epUrl) {
-		return axios.get(API_URL + epUrl).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+		return this.request('get', API_URL + epUrl) ;
 	}
 	delete(epUrl) {
-		return axios.delete(API_URL + epUrl).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+		return this.request('delete', API_URL + epUrl) ;
 	}
-	post(epUrl, data) {
-		console.log(API_URL, epUrl) ;
-		return axios.post(API_URL + epUrl, data, CONTENT_JSON_HEADER).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+
+	// JSON functions
+	post(epUrl, data = null) {
+		return this.request('post', API_URL + epUrl, data, CONTENT_JSON) ;
 	}
-	put(epUrl, data) {
-		return axios.put(API_URL + epUrl, data, CONTENT_JSON_HEADER).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+	put(epUrl, data = null) {
+		return this.request('put', API_URL + epUrl, data, CONTENT_JSON) ;
 	}
-	patch(epUrl, data) {
-		return axios.patch(API_URL + epUrl, data, CONTENT_JSON_HEADER).then(() => this.errClearInternal()).catch((err) => this.errHandlerInternal(err)) ;
+	patch(epUrl, data = null) {
+		return this.request('patch', API_URL + epUrl, data, CONTENT_JSON) ;
+	}
+
+	// FormData functions
+	postFormData(epUrl, data) {
+		if (!(data instanceof FormData)) {
+			console.error('Not a FormData object') ;
+			return null ;
+		}
+		return axios.post(API_URL + epUrl, data) ;
 	}
 }
