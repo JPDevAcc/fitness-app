00becd5c75d773308444c5e1243e62e3a3264e68
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-24 13:42:44 +0000

    Added contact-request/notification test code

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -13,27 +13,36 @@ import { NotificationsLib } from "../libs/notificationsLib";
 // Contexts (global data)
 import { UserContext } from "../contexts/User"
 
+import ContactRequestService from "../services/contactRequestService"; // TEMP: FOR TESTING CONTACT-REQUEST NOTIFICATIONS
+
 // ==============================================================================
 
-export default function Dashboard() {
+export default function Dashboard({viewCommon}) {
+	const contactRequestService = new ContactRequestService(viewCommon.net); // TEMP: FOR TESTING CONTACT-REQUEST NOTIFICATIONS
+
 	const [ userState, dispatch ] = React.useContext(UserContext) ;
 	const notifications = userState.notifications ;
 	const [formattedNotifications, changeFormattedNotifications] = useState([]) ;
 
 	useEffect(() => {
 		console.log("NOTIFICATIONS: ", notifications) ;
-		const newFormattedNotifications = (notifications || []).map(notification => NotificationsLib.formatNotificationData(notification)) ;
+		const newFormattedNotifications = (notifications || []).map(notification => NotificationsLib.formatNotificationData(notification)) ; ///
 		changeFormattedNotifications(newFormattedNotifications) ;
 	}, [notifications]) ;
 
 	return (
-		<div className="page-dashboard">
-			<h1>Dashboard</h1>
-			<div className="my-notifications">
-			{
-				formattedNotifications.map((formattedNotification) => <Notification key={formattedNotification.id} data={formattedNotification} />)
-			}
-			</div> 
-		</div>
+		<>
+			{/* Testing with id for Z@Z.com */}
+			<button onClick={() => contactRequestService.create('641991c078045fab48f32c60')}>TEST: SEND A CONTACT REQUEST TO MYSELF</button>
+
+			<div className="page-dashboard">
+				<h1>Dashboard</h1>
+				<div className="my-notifications">
+				{
+					formattedNotifications.map((formattedNotification) => <Notification key={formattedNotification.id} data={formattedNotification} />)
+				}
+				</div> 
+			</div>
+		</>
   )
 }
