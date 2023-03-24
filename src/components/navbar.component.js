fd86003f97b247cce3f173688796a75358991dbf
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-03-24 08:02:57 +0000

    Navbar improvements

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,24 +1,24 @@
 import "./css/navbar.scss" ;
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import logo from "./Images/logo.png";
+import { NavDropdown } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function NavigationBar(props) {
+	const navigate = useNavigate() ;
   return (
-    <Navbar expand="lg" className="component-navbar navbar-dark bg-dark">
+    <Navbar expand="lg" bg="dark" variant="dark" className="component-navbar">
       <Container fluid>
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="/#">
           {" "}
           Vitality Network{" "}
           <img
             src={logo}
             alt="Logo"
             className="logo"
-            style={{ filter: "invert(100%)" }}
           />
         </Navbar.Brand>
 
@@ -26,23 +26,10 @@ function NavigationBar(props) {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Link className="nav-link" to="/">
-              Home
-            </Link>
-            {/* <Link className="nav-link" to="/register">
-              Register
-            </Link> */}
-            <Link className="nav-link" to="/prefs">
-              Site Prefs
-            </Link>
-            <Link className="nav-link" to="/profile/main">
-              Profile
-            </Link>
-            <Link className="nav-link" to="/account">
-              Account Settings
+              Dashboard
             </Link>
             <Link className="nav-link" to="/recipe">
               Recipes
@@ -51,17 +38,34 @@ function NavigationBar(props) {
               Workouts
             </Link>
 
-            <Button onClick={props.logout}>Logout</Button>
+						<hr class="hr-white" />
+
+						<Link className="d-lg-none nav-link" to="/prefs">
+							Preferences
+						</Link>
+						<Link className="d-lg-none nav-link" to="/profile/main">
+							Profile
+						</Link>
+
+						<hr class="hr-white" />
+
+						<Link className="d-lg-none nav-link" to="/account">
+							Account Settings
+						</Link>
+						<Link className="d-lg-none nav-link" onClick={props.logout}>
+							Logout
+						</Link>			
           </Nav>
-          {/* <Form className="d-flex" >
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form> */}
+
+					 <NavDropdown className="d-none d-lg-block text-white" align="end" flip="true" title={props.userIdentifier}>
+            
+							<NavDropdown.Item onClick={() => navigate("prefs")}>Preferences</NavDropdown.Item>
+							<NavDropdown.Item onClick={() => navigate("profile/main")}>Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+							<NavDropdown.Item onClick={() => navigate("account")}>Account Settings</NavDropdown.Item>
+							<NavDropdown.Item onClick={props.logout}>Logout</NavDropdown.Item>
+            </NavDropdown>
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
